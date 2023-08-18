eff477439879eded569ce34ad86e9583da5e4850
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-08-18 22:44:52 +0300

    added quiz preferences selection + removed navbar

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// HTML Elements
+const numberOfQuestions = document.querySelector('input#number-of-questions');
+const categoriesDropdown = document.querySelector('select#category');
+const difficultyDropdown = document.querySelector('select#difficulty');
+const typeDropdown = document.querySelector('select#type');
+
+// Events
+document.querySelector('button#start-button').addEventListener('click', function () {
+    // TODO: handle starting quiz logic
+});
