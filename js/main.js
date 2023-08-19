92ee3cc0d91dca7a0aff01d570e462f5104e77b4
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-08-19 15:02:47 +0300

    fetching quiz data and redirect to quiz page

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ const difficultyDropdown = document.querySelector('select#difficulty');
 const typeDropdown = document.querySelector('select#type');
 
 // Events
-document.querySelector('button#start-button').addEventListener('click', function () {
-    // TODO: handle starting quiz logic
-    location.replace('./html/quiz.html');
+document.querySelector('button#start-button').addEventListener('click', async function () {
+    if (Number(numberOfQuestions.value) < 1) {
+        return;
+    }
+    try {
+        const response = await fetch('https://opentdb.com/api.php?' + new URLSearchParams({
+            amount: numberOfQuestions.value,
+            category: categoriesDropdown.value,
+            difficulty: difficultyDropdown.value,
+            type: typeDropdown.value,
+        }));
+        sessionStorage.setItem('questions', JSON.stringify(await response.json()));
+        location.replace('./html/quiz.html');
+    } catch (error) {
+        console.log('error');
+    }
 });
