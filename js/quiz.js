0d6aca25e3776bdb0f43992ebdb246885f2fb1e4
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-08-22 12:02:33 +0300

    finish quiz app

--- js/quiz.js
+++ js/quiz.js
@@ -48,20 +48,28 @@ class Question {
 }
 
 // HTML Elements
-let questions = [];
 const category = document.querySelector('h3#category');
 const question = document.querySelector('h3#question');
 const answersSection = document.querySelector('div#answers');
 const difficulty = document.querySelector('h2#difficulty');
-let progress = document.querySelector('span#progress');
-let preventClick = document.querySelector('div#prevent-click');
-let scoreText = document.querySelector('span#score');
+const progress = document.querySelector('span#progress');
+const preventClick = document.querySelector('div#prevent-click');
+const scoreText = document.querySelector('span#score');
+const confettiCanvas = document.querySelector('canvas#canvas');
+const finishScore = document.querySelector('span#finish-score');
+const newQuizButton = document.querySelector('button#new-quiz-button');
+let questions = [];
 let answers;
 
 // Variables
 const data = JSON.parse(sessionStorage.getItem('questions'));
 let index = 0;
 let score = 0;
+
+// Events
+newQuizButton.addEventListener('click', function () {
+    location.replace('../index.html');
+});
 
 // Functions
 function setQuestionData() {
@@ -90,12 +98,19 @@ function handleAnswers() {
                 this.classList.replace('bg-body-tertiary', 'bg-danger');
             }
             preventClick.classList.remove('d-none');
-            setTimeout(() => {
-                index++;
-                if (index >= questions.length) {
-                    // TODO: handle celebration and selecting new preferences
-                    return;
+            index++;
+            if (index >= questions.length) {
+                finishScore.innerHTML = score;
+                confettiCanvas.classList.replace('d-none', 'd-block');
+                if (score > questions.length / 2) {
+                    finishScore.classList.add('text-success');
+                    initConfetti();
+                    render();
                 }
+                showResults();
+                return;
+            }
+            setTimeout(() => {
                 setQuestionData();
                 preventClick.classList.add('d-none');
             }, 1000);
@@ -103,4 +118,11 @@ function handleAnswers() {
     }
 }
 
-setQuestionData();
+function showResults() {
+    document.querySelector('div#finish-overlay').classList.replace('d-none', 'd-flex');
+    for (opacity = 0; opacity <= 1.0; opacity = opacity + 0.1) {
+        setTimeout(function () { document.querySelector('div#finish-overlay').style.opacity = opacity; }, 100);
+    }
+}
+
+(function () { setQuestionData(); })();
